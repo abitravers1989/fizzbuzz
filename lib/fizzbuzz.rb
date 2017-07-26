702f32397a1a683851f0fe03428aa66df00808af
abitravers1989 <atravers4007@gmail.com>
2017-07-26 11:06:52 +0100

    Next step of Fizzbuzz, making it so fizz appears for any number which is divisible by 5, expect 15

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -3,7 +3,7 @@ require 'rspec'
 def fizzbuzz(number)
   if number == 15
     'fizzbuzz'
-  elsif number == 5
+  elsif number %5 == 0
     'buzz'
   elsif number % 3 == 0
         'fizz'
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,8 +4,8 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed a number which is divisible by 3' do
     expect(fizzbuzz(9)).to eq'fizz'
   end
-  it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq'buzz'
+  it 'returns "buzz" when passed a number which is divisible by 5' do
+    expect(fizzbuzz(10)).to eq'buzz'
   end
   it 'returns "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
